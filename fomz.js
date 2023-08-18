e4d0650539009ee0eb05fefec9e7fd09fd35facb
Shi Yu <4763852@qq.com>
2023-08-18 15:24:34 +0800

    Update fomz.js

--- fomz.js
+++ fomz.js
@@ -1,1 +1,14 @@
+/*************************************
+
+项目名称：日杂相机，Fomz相机
+
+**************************************
+
+[rewrite_local]
+^https:\/\/.*\.imendon\.com\/v2\/purchase\/vip\/verification url script-response-body https://raw.githubusercontent.com/bdyxshy/bdyxshy.github.io/master/fomz.js
+
+[mitm]
+hostname = *.imendon.com
+
+*************************************/
 var youxiu = JSON.parse($response.body);youxiu.data.isValid = 1;youxiu.data.expiredTs = 4102415999;$done({body : JSON.stringify(youxiu)});

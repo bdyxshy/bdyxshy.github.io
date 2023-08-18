7964345c44c09237af16f54fd98d088c7bae822f
Shi Yu <4763852@qq.com>
2023-08-18 18:10:46 +0800

    Update fomz.js

--- fomz.js
+++ fomz.js
@@ -1,22 +1,28 @@
-/*************************************
+/******************************
 
-项目名称：日杂相机，Fomz相机
+脚本功能：日杂相机——解锁VIP
+软件版本：1.3.2
 
-**************************************
+*******************************
 
 [rewrite_local]
-^https:\/\/.*\.imendon\.com\/v2\/purchase\/vip\/verification url script-response-body https://raw.githubusercontent.com/bdyxshy/bdyxshy.github.io/master/fomz.js
-^https:\/\/.*\.imendon\.com\/v2\/advert\/homelist url reject
+
+^https:\/\/rzres86api\.imendon\.com\/v2\/purchase\/vip\/verification url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/rzxj.js
+
+^https:\/\/rzres86api\.imendon\.com\/v2\/advert\/homelist url reject
+
 ^https:\/\/api-access\.pangolin-sdk-toutiao\.com\/api\/ad\/union\/sdk\/stats\/batch\/?aid=5000546&version_code=4\.8\.0\.8&device_platform=iphone url reject
 
-[mitm]
-hostname = *.imendon.com
 
-// var youxiu = JSON.parse($response.body);youxiu.data.isValid = 1;youxiu.data.expiredTs = 4102415999;$done({body : JSON.stringify(youxiu)});
-// var youxiu ='{"code":1,"msg":"ok","data":{"isValid":1,"expiredTs":4102415999}}';$done(JSON.stringify(youxiu));
 
-// body = $response.body.replace(/\"isValid":\d+/g, '\"isValid":1')
-// body = $response.body.replace(/\"expiredTs":\d+/g, '\"expiredTs":4102415999')
-// $done({body});
-*************************************/
-alter("嘿嘿嘿");
+[mitm] 
+
+hostname = rzres86api.imendon.com,rzres86api.imendon.com,api-access.pangolin-sdk-toutiao.com
+
+
+
+*******************************/
+
+body = $response.body.replace(/\"isValid":\d+/g, '\"isValid":1')
+
+$done({body});
